a223d4ff61f26c6f7cc89800c2a67deb0b3c49f1
Edward Eskew <edward.eskew@wsu.edu>
2023-02-12 13:47:48 -0800

    Add cli.py

---
+++ src/eeskew_pwg_test_000/cli.py
@@ -0,0 +1,12 @@
+import argparse
+
+from eeskew_pwg_test_000.utils import sarcastic_cowsay
+
+def main():
+    """Cowsay something sarcastically from the command line."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("speech")
+    args = parser.parse_args()
+    
+    s = args.speech
+    sarcastic_cowsay(s)
