28142c0eb732aa2120791415ef97be4dde25bcfd
Edward Eskew <edward.eskew@wsu.edu>
2023-02-12 13:19:12 -0800

    Add sarcastic_cowsay to sarcasm.py

--- src/eeskew_pwg_test_000/sarcasm.py
+++ src/eeskew_pwg_test_000/sarcasm.py
@@ -1,3 +1,5 @@
+import cowsay
+
 def sarcasm(s):
     """Convert string `s` to sArCaSm TeXt."""
     out = ""
@@ -9,3 +11,8 @@ def sarcasm(s):
             out += c.upper()
 
     return out
+
+def sarcastic_cowsay(s):
+    """Cowsay `s`, sArCaStIcAlLy."""
+    sarcastic_s = sarcasm(s)
+    cowsay.cow(sarcastic_s)
