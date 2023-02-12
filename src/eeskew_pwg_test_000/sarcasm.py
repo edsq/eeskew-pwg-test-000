0fb6cb10ff691591da9c321a6dbb99b97dec9571
Edward Eskew <edward.eskew@wsu.edu>
2023-02-12 13:22:33 -0800

    Use bad formatting in sarcasm.py for black to fix

--- src/eeskew_pwg_test_000/sarcasm.py
+++ src/eeskew_pwg_test_000/sarcasm.py
@@ -1,18 +1,22 @@
 import cowsay
-
-def sarcasm(s):
+def sarcasm(
+            s
+          ):
     """Convert string `s` to sArCaSm TeXt."""
-    out = ""
-    for i, c in enumerate(s):
-        if i % 2 == 0:
-            out += c.lower()
+    out = ''
+    for i,c in \
+        enumerate( s ):
+
+        if i% 2 ==0: out +=c.lower()
 
         else:
-            out += c.upper()
+
+
+
+            out+= c.upper()
 
     return out
 
 def sarcastic_cowsay(s):
     """Cowsay `s`, sArCaStIcAlLy."""
-    sarcastic_s = sarcasm(s)
-    cowsay.cow(sarcastic_s)
+    sarcastic_s = sarcasm(s); cowsay.cow(sarcastic_s)
